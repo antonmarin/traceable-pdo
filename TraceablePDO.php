0a1bf131b80b4b5e936f6a0a123193c84f1e5f4d
Anton Marin <antonaryo85@gmail.com>
2017-01-17 14:37:06 +0300

    Create TraceablePDO.php

---
+++ TraceablePDO.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace traceablePdo;
+
+/**
+ * PDO подключение к базе
+ *
+ * Добавляет трейс к запросу
+ */
+class TraceablePDO extends \PDO
+{
+    /** @var int Depth of trace */
+    public $traceLevel = 7;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @internal Overrided to add trace
+     */
+    public function prepare($statement, array $driver_options = array())
+    {
+        $trace = $this->collectTrace();
+        $trace = empty($trace) ? '' : "\n    " . implode("\n    ", $trace);
+        $statement .= '/* ' . base64_encode(gzcompress($trace)) . ' */';
+        return parent::prepare($statement, $driver_options);
+    }
+
+    /**
+     * get trace of statement
+     * @return array Массив строк с указанием на строку файла
+     */
+    private function collectTrace()
+    {
+        $traces = [];
+        $count = 0;
+        $ts = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        array_shift($ts); // first is ->prepare() it is not useful
+        array_pop($ts); // remove the last trace since it would be the entry script, not very useful
+        foreach ($ts as $trace) {
+            if (
+                isset($trace['file'], $trace['line'])
+                && (!defined('YII2_PATH') || strpos($trace['file'], YII2_PATH) !== 0)
+            ) {
+                $traces[] = "#$count {$trace['file']}:{$trace['line']}";
+                if (++$count >= $this->traceLevel) {
+                    break;
+                }
+            }
+        }
+
+        return $traces;
+    }
+}
